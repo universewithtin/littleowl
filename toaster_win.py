6e8bb8acab45cac911ff47f848bc4349dfebe077
SPC <nurmukhammed.smatayev@gmail.com>
2024-08-08 23:00:46 +0500

    ADD: audio for toast on windows

--- toaster_win.py
+++ toaster_win.py
@@ -1,11 +1,11 @@
 from winotify import Notification, audio
-
+import os
 
 def toaster(title, msg):
     toast = Notification(app_id="LittleOwl",
                          title=title,
                          msg=msg,
                          duration="long")
+    
+    toast.set_audio(audio.LoopingAlarm4, loop=True)
     toast.show()
-
-
